a02f642cb8168c01b24b6116fac2a3f8eb543c0b
DnYorch213 <joloare@hotmail.com>
2023-05-15 20:04:00 -0600

    version 7

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,27 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
  //const socket = io("http://localhost:4000");
-const socket = io("/");
+const socket = io ("/");
 
 export default function App() {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
+    let data = localStorage.getItem("messages");
+    if (data) {
+      setMessages(JSON.parse(data));
+    }
+    
+  }, []); 
+
+  useEffect(() => {
+    localStorage.setItem("messages", JSON.stringify(messages));
+  }, [messages]); 
+
+  useEffect(() => {
     const receiveMessage = (message) => {
-      setMessages([message, ...messages]);
+      setMessages([...messages, message]);
     };
 
     socket.on("message", receiveMessage);
@@ -26,7 +38,7 @@ export default function App() {
       body: message,
       from: "Yo",
     };
-    setMessages([newMessage, ...messages]);
+    setMessages([...messages, newMessage]);
     setMessage("");
     socket.emit("message", newMessage.body);
   };
@@ -35,15 +47,7 @@ export default function App() {
     <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
       <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
         <h1 className="text-2xl font-bold my-2">Chat de Dn.Yorch!</h1>
-        <input
-          name="message"
-          type="text"
-          placeholder="Write your message..."
-          onChange={(e) => setMessage(e.target.value)}
-          className="border-2 border-zinc-500 p-2 w-full text-black"
-          value={message}
-          autoFocus
-        />
+        
 
         <ul className="h-80 overflow-y-auto">
           {messages.map((message, index) => (
@@ -57,6 +61,16 @@ export default function App() {
             </li>
           ))}
         </ul>
+
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
       </form>
     </div>
   );
